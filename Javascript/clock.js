92281322cd08b7bad693a290764a72f4fd291a2a
Sachin Hasija <sachin.khms@gmail.com>
2019-04-15 20:33:12 +0530

    initial add files commit

---
+++ Javascript/clock.js
@@ -0,0 +1,43 @@
+function showTime() {
+  var canvas = document.querySelector('#canvas');
+  var context = canvas.getContext('2d');
+  var clockRadius = canvas.width / 2;
+  context.fillStyle = 'rgb(111, 48, 21)';
+
+  context.beginPath();
+  context.arc(clockRadius, clockRadius, clockRadius, 0, 2 * Math.PI);
+  context.fill();
+  context.beginPath();
+  context.arc(clockRadius, clockRadius, clockRadius / 40, 0, 2 * Math.PI);
+  context.fillStyle = 'rgb(181, 189, 184)';
+  context.fill();
+  context.font = clockRadius / 10 + 'px Sans-Serif';
+  context.fillStyle = 'rgb(38, 22, 0)';
+  for (let i = 1; i <= 12; i++) {
+    context.fillText(i, clockRadius - clockRadius / 20 + clockRadius * 0.9 * Math.sin(i * 2 * Math.PI / 12), clockRadius - clockRadius * 0.9 * Math.cos(i * 2 * Math.PI / 12));
+  }
+
+  var hours = new Date().getHours();
+  var minutes = new Date().getMinutes();
+  var seconds = new Date().getSeconds();
+  var hour = hours % 12 + minutes / 60 + seconds / 3600;
+  var hourAngle = hour * 2 * Math.PI / 12;
+  var minuteAngle = minutes * 2 * Math.PI / 60;
+  var secondAngle = seconds * 2 * Math.PI / 60;
+  context.strokeStyle = 'rgb(181, 189, 184)';
+  context.moveTo(clockRadius, clockRadius);
+  context.lineTo(clockRadius + clockRadius * 0.5 * Math.sin(hourAngle), clockRadius - clockRadius * 0.5 * Math.cos(hourAngle));
+  context.lineWidth = 7;
+  context.stroke();
+  context.moveTo(clockRadius, clockRadius);
+  context.lineTo(clockRadius + clockRadius * 0.7 * Math.sin(minuteAngle), clockRadius - clockRadius * 0.7 * Math.cos(minuteAngle));
+  context.lineWidth = 4;
+  context.stroke();
+  context.moveTo(clockRadius, clockRadius);
+  context.lineTo(clockRadius + clockRadius * 0.9 * Math.sin(secondAngle), clockRadius - clockRadius * 0.9 * Math.cos(secondAngle));
+  context.lineWidth = 2;
+  context.stroke();
+  context.strokeStyle = 'black';
+}
+
+setInterval(showTime, 1000);
